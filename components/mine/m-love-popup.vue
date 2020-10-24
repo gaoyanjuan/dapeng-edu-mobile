0cac4e7b2b6d8e912adce5e14ad56a08036dd997
济南-王超 <2386809844@qq.com>
2020-10-24 14:37:47 +0800

    feat：我的-我的喜欢弹框

---
+++ components/mine/m-love-popup.vue
@@ -0,0 +1,66 @@
+<template>
+  <van-popup v-model="showPopup.show" round>
+    <div class="popup-warp">
+      <div class="popup-head-photo"></div>
+      <div class="popup-body-nums">累计获得了2341266个赞</div>
+      <div class="popup-footer-btn" @click="showPopup.show = false">知道啦</div>
+    </div>
+  </van-popup>
+</template>
+
+<script>
+export default {
+  name:'M-Love-Popup',
+  props:{
+    showPopup:{
+      type: Object,
+      default: {}
+    }
+  },
+}
+</script>
+
+<style lang="less" scoped>
+
+.popup-warp {
+  width: 260px;
+  height: 235px;
+  background: linear-gradient(182deg, #FFFFFF 0%, #F7F9F7 100%);
+  border-radius: 8px;
+  overflow: hidden;
+
+  & .popup-head-photo {
+    width: 100%;
+    height: 98px;
+    background-color: transparent;
+    background-image: url('~@/assets/icons/mine/love-popup-bg.png');
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    background-position: center;
+  }
+
+  & .popup-body-nums {
+    width: 100%;
+    height: 89px;
+    font-size: 16px;
+    font-family: @dp-font-semibold;
+    font-weight: 600;
+    color: #36404A;
+    line-height: 89px;
+    text-align: center;
+    border-bottom: 1px solid #F0F0F8;
+  }
+
+  & .popup-footer-btn {
+    width: 100%;
+    height: 47px;
+    font-size: 14px;
+    font-family: @dp-font-regular;
+    font-weight: 400;
+    color: #333333;
+    line-height: 47px;
+    text-align: center;
+    cursor: pointer;
+  }
+}
+</style>
