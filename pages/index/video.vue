8eb4bcb027c56f6d97268cec8a87d0afa6dccbbc
济南-王超 <2386809844@qq.com>
2020-11-18 18:11:24 +0800

    feat：视频列表渲染

--- pages/index/video.vue
+++ pages/index/video.vue
@@ -5,9 +5,25 @@
 <script>
 export default {
   async asyncData ({route, store}) {},
-  created () {},
-  watch: {},
-  destroyed () {},
-  methods:{}
+  created () {
+    if (process.browser) {
+      if (this.$store.getters['colleges/videoCollegesGetters'].length === 1) {
+        this.$store.dispatch('colleges/appendColleges', { collegeType : 'SQUARE_MOVIE' })
+      }
+      if (this.$store.getters['video/videoListGetters'].list.length === 0) {
+        this.$store.dispatch('video/appendVideoList', { collegeId: this.$route.query.college, page: 1 })
+      }
+    }
+  },
+  watch: {
+    '$route.query': function (newQuery, oldQuery) {
+      this.$store.commit('video/clearVideoList')
+      this.$store.dispatch('video/appendVideoList', { collegeId: newQuery.college, page: 1 })
+    }
+  },
+  methods:{},
+  destroyed () {
+    this.$store.commit('video/clearVideoList')
+  },
 }
 </script>
