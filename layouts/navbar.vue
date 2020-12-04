995c6c376ca17656f92d54fd2c61c14f249d41e1
济南-王超 <2386809844@qq.com>
2020-12-04 13:18:27 +0800

    feat：完善无网络文案优化

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -4,7 +4,8 @@
       <Nuxt />
     </div>
     <div class="off-line-wrap" v-if="$nuxt.isOffline">
-      请检查网络
+       <span class="off-line-txt"> 您的网络已断开 </span>
+       <span class="off-line-txt"> 请检查后刷新一下哦~ </span>
     </div>
   </div>
 </template>
@@ -20,7 +21,21 @@
 .off-line-wrap {
   display: flex;
   align-items: center;
-  justify-content: center;
+  flex-direction: column;
   margin: 45% auto;
 }
+
+
+.off-line-txt {
+  font-size: 14px;
+  font-family: @regular;
+  font-weight: 400;
+  color: #747C80;
+  line-height: 26px;
+}
+
+.off-line-txt:not(:first-child) {
+  margin-top: 5px;
+}
+
 </style>
