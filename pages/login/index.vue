fbab3e6dac59c3e086c5f9132da0b4d703b284fe
济南-王鹤潼 <wht@DESKTOP-1QSSTAK.localdomain>
2020-12-08 14:30:17 +0800

    feat:优化登录,callback重定向

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -30,11 +30,9 @@ export default {
       height: 0
     }
   },
-  created () {
-    if (process.browser) {
-      if (this.$cookiz.get('access_token')) {
-        this.$router.replace('/personal-center')
-      }
+  async asyncData ({ redirect, app: { $cookiz } }) {
+    if ($cookiz.get('access_token')) {
+      redirect('/')
     }
   },
   mounted () {
