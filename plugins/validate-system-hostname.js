39cd9cd85c297af07a2c323200c3d0893a654e58
cbh <470584371@qq.com>
2020-12-14 13:44:30 +0800

    feat：移动端统一使用pc4.0的client信息

--- plugins/validate-system-hostname.js
+++ plugins/validate-system-hostname.js
@@ -1,18 +1,25 @@
 function validateSystemHostName () {
   const localPCParam = {
-    client_id: 'local-m5',
-    client_secret: 'secret'
+    client_id: 'pc',
+    client_secret: 'COm6AqoH'
   }
   const testPCParam = {
-    client_id: 'e36c78fd',
-    client_secret: 'x8BS3cGDiz'
+    client_id: 'b4cd9c34',
+    client_secret: 'VXAaTFh8yv'
+  }
+  const prodPCParam = {
+    client_id: 'Dd8fbbB5',
+    client_secret: 'tz9Yfdtn6w'
   }
   if (process.browser) {
     localPCParam.host = `${location.protocol}//${location.host}`
     testPCParam.host = `${location.protocol}//${location.host}`
+    prodPCParam.host = `${location.protocol}//${location.host}`
   }
   if (process.env.mode === 'test') {
     return testPCParam
+  } else if (process.env.mode === 'production') {
+    return prodPCParam
   } else {
     return localPCParam
   }
