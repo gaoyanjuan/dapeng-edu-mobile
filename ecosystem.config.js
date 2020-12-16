486c4cd17b645af9505a0ca4b688249e1ea3479d
济南-王鹤潼 <wht@DESKTOP-1QSSTAK.localdomain>
2020-12-16 16:09:46 +0800

    feat:修改pm2配置,修改接口地址配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,27 @@
 module.exports = {
   apps: [
     {
-      name: 'M-5.1-test',
+      name: 'wap-pro',
       exec_mode: 'cluster',
       instances: 'max', // Or a number of instances
-      max_memory_restart:'600M',
+      max_memory_restart:'1000M',
       script: './node_modules/nuxt/bin/nuxt.js',
       args: 'start',
-      error_file: "./logs/app-err.log",
-      out_file: "./logs/app-out.log",
+      error_file: "/home/dapeng/logs/pm2/wap-err.log",
+      out_file: "/home/dapeng/logs/pm2/wap-out.log",
+      env: {
+        "MODE": "pro"
+      }
+    },
+    {
+      name: 'wap-test',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      max_memory_restart:'1000M',
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+      error_file: "./logs/wap-err.log",
+      out_file: "./logs/wap-out.log",
       env: {
         "MODE": "test"
       }
