0ebd650540a147269fd2046b3251207067df4466
济南-王超 <2386809844@qq.com>
2020-12-19 04:00:33 +0800

    fix：修复提交作业接口延迟出现的数据问题

--- pages/homework-list/index.vue
+++ pages/homework-list/index.vue
@@ -118,6 +118,8 @@ export default {
       })
     }
 
+    this.$store.commit('homework/clearOpenCourses')
+
     if(this.course.list.length === 0) {
       this.appendOpenCourses({
         collegeId: this.$route.query.college || '',
