c3288760c940e7882558e8f7237c53c55b2279d9
济南-王鹤潼 <wht@DESKTOP-1QSSTAK.localdomain>
2021-01-03 15:16:29 +0800

    feat:修改axios配置

--- utils/server-axios.js
+++ utils/server-axios.js
@@ -1,24 +1,20 @@
 const validateSystemHostName = require('../plugins/validate-system-hostname')
 const axios = require('axios')
-const baseURL = validateSystemHostName.default().baseURL
 const HttpAgent = require('agentkeepalive')
 const HttpsAgent = require('agentkeepalive').HttpsAgent
 
 const instance = axios.create({
-  baseURL,
-  timeout: 4000
-})
-
-instance.httpAgent = new HttpAgent({
-  keepAlive: true,
-  freeSocketTimeout: 4000,
-  maxFreeSockets: 256
-})
-
-instance.httpsAgent = new HttpsAgent({
-  keepAlive: true,
-  freeSocketTimeout: 4000,
-  maxFreeSockets: 256
+  timeout: 4000,
+  httpAgent: new HttpAgent({
+    keepAlive: true,
+    freeSocketTimeout: 4000,
+    maxFreeSockets: 256
+  }),
+  httpsAgent: new HttpsAgent({
+    keepAlive: true,
+    freeSocketTimeout: 4000,
+    maxFreeSockets: 256
+  })
 })
 
 module.exports = instance
