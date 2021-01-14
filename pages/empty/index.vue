91406d605d9b1b774632b8796aae1779a492e315
济南-王超 <2386809844@qq.com>
2021-01-14 18:58:46 +0800

    feat：3.0 提交作业完善

---
+++ pages/empty/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="empty-wrapper">
+
+    <m-navbar title="空白页"></m-navbar>
+
+    <div class="empty">
+      <img class="empty-icon" :src="empty" alt="empty" />
+      <span class="empty-txt">暂无可提交作业～</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'Empty',
+
+  layout:'navbar',
+
+  data: ()=> ({
+    empty: require('@/assets/icons/blank/have-no-homework.png')
+  })
+}
+</script>
+
+<style lang="less" scoped>
+.empty-wrapper {
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  background: @dp-white;
+}
+
+.empty-wrapper .empty {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+
+  &-icon {
+    width: 240px;
+    height: 126px;
+  }
+
+  &-txt {
+    font-size: 14px;
+    font-family: @semibold;
+    font-weight: 600;
+    color: #8D8E8E;
+    margin-top: 12px;
+    text-align: center;
+  }
+}
+</style>
