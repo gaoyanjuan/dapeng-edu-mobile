f73d686529c1623aa6c036fffeb9fdebcd0abb98
gaoyanjuan <gyj18194228750@163.com>
2021-02-20 13:38:49 +0800

    fix:解决冲突

--- nuxt.config.js
+++ nuxt.config.js
@@ -132,17 +132,17 @@ export default {
     credentials: true // 表示跨域请求时是否需要使用凭证
   },
   proxy: {
-<<<<<<< HEAD
     '/api/part_job': {
       target: 'https://m.680.com/api',
       pathRewrite: {
-        '^/api/part_job': '/',
-=======
+        '^/api/part_job': '/'
+      }
+    },
+
     '/api/buriedPoint/log': {
       target: env[process.env.MODE].LOG_API_URL, // 日志接口调用地址
       pathRewrite: {
         '^/api/buriedPoint/log': '/api/buriedPoint/log',
->>>>>>> e079b52df433dcb87f381e5985abc1eba8684972
         changeOrigin: true
       }
     },
